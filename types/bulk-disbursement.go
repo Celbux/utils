acd350bd9c11ac3771989cd3a71128b521dee195
James Wilson <mspacedev1@gmail.com>
2020-10-13 21:03:15 +0200

    Added bulk disbursement structs

--- types/bulk-disbursement.go
+++ types/bulk-disbursement.go
@@ -17,3 +17,27 @@ type Pair struct {
 	Key string `json:"key"`
 	Value string `json:"value"`
 }
+
+type Shard struct {
+	Amount int64
+	Filled int64
+	ErrorChildren []string
+	Children []string
+	ID string
+}
+
+type DisburseRow struct {
+	Amount string `json:"amount"`
+	To string `json:"to"`
+}
+
+type Response struct {
+	Data string `json:"data"`
+	Error string `json:"error"`
+}
+
+
+type Recon struct {
+	TotalDisburse int64
+	TotalDisbursees int
+}
